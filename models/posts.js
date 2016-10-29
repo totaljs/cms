526201f21d764668d0f1df7d736e08814503d5c7
Herve Prot <herve.prot@tomanage.fr>
2016-10-29 17:25:31 +0200

    Add 'meta description' in pages and posts

--- models/posts.js
+++ models/posts.js
@@ -8,6 +8,7 @@ NEWSCHEMA('Post').make(function(schema) {
 	schema.define('perex', 'String(500)');
 	schema.define('template', 'String(50)');
 	schema.define('keywords', 'String(200)');
+        schema.define('description', 'String(250)');// Meta Description
 	schema.define('tags', '[String]');
 	schema.define('search', 'String(1000)');
 	schema.define('pictures', '[String]');  		// URL addresses for first 5 pictures
