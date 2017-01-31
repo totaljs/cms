84aeb7968ee4cc9a6ee71d41afb87dee1e7e55c3
Peter Širka <petersirka@gmail.com>
2017-01-31 21:38:25 +0100

    Improve code.

--- definitions/settings.js
+++ definitions/settings.js
@@ -1,8 +1,8 @@
 // Reads custom settings
 GETSCHEMA('Settings').workflow2('load');
 
-// Disables analytic counter for administraion / manager
-MODULE('webcounter').instance.onValid = req => !req.url.startsWith(CONFIG('manager-url'));
+// Disables an analytic counter for administration / manager
+MODULE('webcounter').blacklist(CONFIG('manager-url'));
 
 // Global static variables (default values)
 F.global.sitemap = [];
