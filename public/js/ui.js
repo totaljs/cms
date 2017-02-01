2e69e59cefd5b124af140cd906100450c5ffdbba
Peter Širka <petersirka@gmail.com>
2017-02-01 23:00:47 +0100

    Improve UI.

--- public/js/ui.js
+++ public/js/ui.js
@@ -1862,7 +1862,7 @@ COMPONENT('calendar', function() {
 	}
 
 	self.hide = function() {
-		self.element.toggleClass('hidden', true);
+		self.classes('hidden');
 		visible = false;
 		return self;
 	};
