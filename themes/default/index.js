b29b9952bdb4ac9b6fc92f59da2d3c19a480c6d8
Jonathan Dobson <bringjd@gmail.com>
2019-01-18 20:10:18 +0800

    Fix paths
    I think that the MERGE method was not working, so I changed the paths for these four ui assets:
     './default/public/js/ui.js', 
    './default/public/js/default.js'
    './default/public/css/ui.css', 
    './default/public/css/default.css'
    Everything is loading fine now

--- themes/default/index.js
+++ themes/default/index.js
@@ -1,4 +1,4 @@
 exports.install = function() {
-	MERGE('/default/js/default.js', '=default/public/js/ui.js', '=default/public/js/default.js');
-	MERGE('/default/css/default.css', '=default/public/css/ui.css', '=default/public/css/default.css');
+	MERGE('/default/js/default.js', './default/public/js/ui.js', './default/public/js/default.js');
+	MERGE('/default/css/default.css', './default/public/css/ui.css', './default/public/css/default.css');
 };
