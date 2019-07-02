78de90e3ac687a5b8cedb6db2b5eec8ecfe597c9
Denis Granec <denis@granec.cz>
2019-07-02 16:29:16 +0200

    Big update.

--- themes/admin/public/ui.js
+++ themes/admin/public/ui.js
@@ -2,35 +2,66 @@ COMPONENT('exec', function(self, config) {
 	self.readonly();
 	self.blind();
 	self.make = function() {
-		self.event('click', config.selector || '.exec', function(e) {
-			var el = $(this);
 
-			var attr = el.attrd('exec');
-			var path = el.attrd('path');
-			var href = el.attrd('href');
+		var scope = null;
 
-			if (el.attrd('prevent') === 'true') {
-				e.preventDefault();
-				e.stopPropagation();
-			}
+		var scopepath = function(el, val) {
+			if (!scope)
+				scope = el.scope();
+			return scope ? scope.makepath ? scope.makepath(val) : val.replace(/\?/g, el.scope().path) : val;
+		};
 
-			attr && EXEC(attr, el, e);
-			href && NAV.redirect(href);
+		var fn = function(plus) {
+			return function(e) {
 
-			if (path) {
-				var val = el.attrd('value');
-				if (val == null) {
-					var a = new Function('return ' + el.attrd('value-a'))();
-					var b = new Function('return ' + el.attrd('value-b'))();
-					var v = GET(path);
-					if (v === a)
-						SET(path, b, true);
-					else
-						SET(path, a, true);
-				} else
-					SET(path, new Function('return ' + val)(), true);
-			}
-		});
+				var el = $(this);
+				var attr = el.attrd('exec' + plus);
+				var path = el.attrd('path' + plus);
+				var href = el.attrd('href' + plus);
+				var def = el.attrd('def' + plus);
+				var reset = el.attrd('reset' + plus);
+
+				scope = null;
+
+				if (el.attrd('prevent' + plus) === 'true') {
+					e.preventDefault();
+					e.stopPropagation();
+				}
+
+				if (attr) {
+					if (attr.indexOf('?') !== -1)
+						attr = scopepath(el, attr);
+					EXEC(attr, el, e);
+				}
+
+				href && NAV.redirect(href);
+
+				if (def) {
+					if (def.indexOf('?') !== -1)
+						def = scopepath(el, def);
+					DEFAULT(def);
+				}
+
+				if (reset) {
+					if (reset.indexOf('?') !== -1)
+						reset = scopepath(el, reset);
+					RESET(reset);
+				}
+
+				if (path) {
+					var val = el.attrd('value');
+					if (val) {
+						if (path.indexOf('?') !== -1)
+							path = scopepath(el, path);
+						var v = GET(path);
+						SET(path, new Function('value', 'return ' + val)(v), true);
+					}
+				}
+			};
+		};
+
+		self.event('dblclick', config.selector2 || '.exec2', fn('2'));
+		self.event('click', config.selector || '.exec', fn(''));
 	};
 });
 
