46b056fc659d548aa12bd01a3991a5d457848b87
Peter Širka <petersirka@gmail.com>
2019-12-18 13:35:58 +0100

    Added new changes.

--- themes/admin/public/ui.js
+++ themes/admin/public/ui.js
@@ -10745,3 +10745,30 @@ COMPONENT('layout', 'space:1;border:0;parent:window;margin:0;remember:1', functi
 	};
 
 });
+
+COMPONENT('selected', 'class:selected;selector:a;attr:if', function(self, config) {
+
+	self.bindvisible(20);
+	self.readonly();
+
+	self.configure = function(key, value) {
+		switch (key) {
+			case 'datasource':
+				self.datasource(value, function() {
+					self.refresh();
+				});
+				break;
+		}
+	};
+
+	self.setter = function(value) {
+		var cls = config.class;
+		self.find(config.selector).each(function() {
+			var el = $(this);
+			if (el.attrd(config.attr) === value)
+				el.aclass(cls);
+			else
+				el.hclass(cls) && el.rclass(cls);
+		});
+	};
+});
