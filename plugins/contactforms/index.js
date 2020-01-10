a854bf0b4b8438d6a4723c3db8de52c8fe4aae69
Peter Sirka <petersirka@gmail.com>
2020-01-10 20:09:49 +0100

    Removed clearing of contact forms.

--- plugins/contactforms/index.js
+++ plugins/contactforms/index.js
@@ -6,6 +6,7 @@ exports.install = function() {
 	ROUTE('GET     /admin/api/contactforms/                    *ContactForms --> @query');
 	ROUTE('GET     /admin/api/contactforms/{id}/               *ContactForms --> @read');
 	ROUTE('DELETE  /admin/api/contactforms/                    *ContactForms --> @remove');
-	ROUTE('GET     /admin/api/contactforms/stats/              *ContactForms --> stats');
+	ROUTE('GET     /admin/api/contactforms/stats/              *ContactForms --> @stats');
+	ROUTE('GET     /admin/api/contactforms/clear/              *ContactForms --> @clear');
 	ROUTE('POST    /api/contact/                               *ContactForms --> @save');
 };
