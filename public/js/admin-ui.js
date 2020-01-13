5287e2768ff3f4088af823e2637edb222e731493
Peter Sirka <petersirka@gmail.com>
2020-01-14 00:09:32 +0100

    Added new improvements.

--- public/js/admin-ui.js
+++ public/js/admin-ui.js
@@ -10421,3 +10421,171 @@ COMPONENT('avatar', function(self) {
 		return (pound ? '#': '') + (g | (b << 8) | (r << 16)).toString(16);
 	};
 });
+
+COMPONENT('colorpicker', function(self) {
+
+	var cls = 'ui-colorpicker';
+	var cls2 = '.' + cls;
+	var is = false;
+	var events = {};
+	var colors = [['e73323', 'ec8632', 'fffd54', '7bfa4c', '7cfbfd', '041ef5', 'e73cf7', '73197b', '91683c', 'ffffff', '808080', '000000'],['ffffff', 'e8e8e8', 'd1d1d1', 'b9b9b9', 'a2a2a2', '8b8b8b', '747474', '5d5d5d', '464646', '2e2e2e', '171717', '000000'],['5c0e07', '5e350f', '66651c', '41641a', '2d6419', '2d6438', '2d6465', '133363', '000662', '2d0962', '5c1262', '5c0f32', '8a1a11', '8e501b', '99982f', '62962b', '47962a', '479654', '479798', '214d94', '010e93', '451393', '8a2094', '8a1c4c', 'b9261a', 'bd6b27', 'cccb41', '83c83c', '61c83b', '61c871', '62c9ca', '2e67c5', '0216c4', '5c1dc4', 'b92ec5', 'b92865', 'e73323', 'ec8632', 'fffd54', 'a4fb4e', '7bfa4c', '7bfa8d', '7cfbfd', '3b80f7', '041ef5', '7327f5', 'e73cf7', 'e7357f', 'e8483f', 'ef9d4b', 'fffe61', 'b4fb5c', '83fa5a', '83faa2', '83fbfd', '5599f8', '343cf5', '8c42f6', 'e84ff7', 'e84a97', 'ea706b', 'f2b573', 'fffe7e', 'c5fc7c', '96fa7a', '96fbb9', '96fcfd', '7bb2f9', '666af6', 'a76ef7', 'eb73f8', 'ea71b0', 'f6cecd', 'fae6cf', 'fffed1', 'ebfed1', 'd7fdd0', 'd7fde7', 'd8fefe', 'd1e5fd', 'cccdfb', 'e1cefb', 'f6cffc', 'f6cee4']];
+
+	self.singleton();
+	self.readonly();
+	self.blind();
+	self.nocompile();
+
+	self.make = function() {
+
+		var html = '';
+		for (var i = 0; i < colors.length; i++) {
+			html += '<div>';
+			for (var j = 0; j < colors[i].length; j++) {
+				html += '<span class="{0}-cell"><span style="background-color:#{1}"></span></span>'.format(cls, colors[i][j]);
+			}
+			html += '</div>';
+		}
+
+		self.html('<div class="{0}"><div class="{0}-body">{1}</div></div>'.format(cls, html));
+		self.aclass(cls + '-container hidden');
+
+		self.event('click', cls2 + '-cell', function() {
+			var el = $(this);
+			self.opt.callback && self.opt.callback(el.find('span').attr('style').replace('background-color:', ''));
+			self.hide();
+		});
+
+		events.click = function(e) {
+			var el = e.target;
+			var parent = self.dom;
+			do {
+				if (el == parent)
+					return;
+				el = el.parentNode;
+			} while (el);
+			self.hide();
+		};
+
+		self.on('scroll + reflow', self.hide);
+	};
+
+	self.bindevents = function() {
+		if (!events.is) {
+			events.is = true;
+			$(document).on('click', events.click);
+		}
+	};
+
+	self.unbindevents = function() {
+		if (events.is) {
+			events.is = false;
+			$(document).off('click', events.click);
+		}
+	};
+
+	self.show = function(opt) {
+
+		var tmp = opt.element ? opt.element instanceof jQuery ? opt.element[0] : opt.element.element ? opt.element.dom : opt.element : null;
+
+		if (is && tmp && self.target === tmp) {
+			self.hide();
+			return;
+		}
+
+		events.is && self.unbindevents();
+		self.target = tmp;
+		self.opt = opt;
+		var css = {};
+
+		if (is) {
+			css.left = 0;
+			css.top = 0;
+			self.element.css(css);
+		} else
+			self.rclass('hidden');
+
+		var target = $(opt.element);
+		var w = self.element.width();
+		var offset = target.offset();
+
+		if (opt.element) {
+			switch (opt.align) {
+				case 'center':
+					css.left = Math.ceil((offset.left - w / 2) + (target.innerWidth() / 2));
+					break;
+				case 'right':
+					css.left = (offset.left - w) + target.innerWidth();
+					break;
+				default:
+					css.left = offset.left;
+					break;
+			}
+
+			css.top = opt.position === 'bottom' ? (offset.top - self.element.height() - 10) : (offset.top + target.innerHeight() + 10);
+
+		} else {
+			css.left = opt.x;
+			css.top = opt.y;
+		}
+
+		if (opt.offsetX)
+			css.left += opt.offsetX;
+
+		if (opt.offsetY)
+			css.top += opt.offsetY;
+
+		is = true;
+		self.element.css(css);
+		setTimeout(self.bindevents, 10);
+	};
+
+	self.hide = function() {
+		if (is) {
+			is = false;
+			self.target = null;
+			self.opt = null;
+			setTimeout(self.unbindevents, 50);
+			self.aclass('hidden');
+		}
+	};
+});
+
+COMPONENT('colorpickerbutton', 'default:#FFFFFF;align:left;position:top', function(self, config) {
+
+	var cls = 'ui-colorpickerbutton';
+	var cls2 = '.' + cls;
+
+	self.nocompile();
+
+	self.make = function() {
+		self.aclass(cls);
+		self.append('<span class="{0}-arrow"><i class="fa fa-angle-down"></i></span><div class="{0}-color"></div>'.format(cls));
+		self.event('click', function() {
+			if (config.disabled)
+				return;
+			var opt = {};
+			opt.align = config.align;
+			opt.position = config.position;
+			opt.offsetX = config.offsetX;
+			opt.offsetY = config.offsetY;
+			opt.element = self.element;
+			opt.callback = function(color) {
+				self.set(color.toUpperCase());
+				self.change(true);
+			};
+			SETTER('colorpicker', 'show', opt);
+		});
+	};
+
+	self.configure = function(key, value) {
+		switch (key) {
+			case 'disabled':
+				self.tclass('ui-disabled', !!value);
+				break;
+		}
+	};
+
+	self.setter = function(value) {
+		self.find(cls2 + '-color').css('background-color', value || config.default);
+	};
+});
