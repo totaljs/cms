8caf141c37b5523cd9be233dac60c6e1044b9b22
Peter Širka <petersirka@gmail.com>
2020-01-17 09:27:32 +0100

    Clean routes.

--- controllers/default.js
+++ controllers/default.js
@@ -1,59 +1,9 @@
 exports.install = function() {
-
 	// Enable CORS for API
 	CORS();
-
 	ROUTE('/*', view_cms);
-
-	// ROUTE('#posts',   view_posts,        ['*Posts']);
-	// ROUTE('#post',    view_posts_detail, ['*Posts']);
-	// ROUTE('#notices', view_notices,      ['*Notices']);
 };
 
 function view_cms() {
 	this.CMSpage();
-}
-
-function view_posts() {
-	var self = this;
-	var options = {};
-
-	options.page = self.query.page;
-	options.published = true;
-	options.limit = 10;
-	// options.category = 'category_linker';
-
-	self.sitemap();
-	self.$query(options, self.callback('posts'));
-}
-
-function view_posts_detail(linker) {
-
-	var self = this;
-	var options = {};
-
-	options.linker = linker;
-	// options.category = 'category_linker';
-
-	self.$workflow('render', options, function(err, response) {
-
-		if (err) {
-			self.throw404();
-			return;
-		}
-
-		self.sitemap();
-		self.sitemap_replace(self.sitemapid, response.name);
-		self.view('cms/' + response.template, response);
-	});
-}
-
-function view_notices() {
-	var self = this;
-	var options = {};
-
-	options.published = true;
-
-	self.sitemap();
-	self.$query(options, self.callback('notices'));
 }
