91bcdf45b217ca0cc9608952dbf57a69c17a8998
Peter Širka <petersirka@gmail.com>
2024-02-13 17:10:19 +0100

    Updated script.

--- bundle.sh
+++ bundle.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-require('total4');
+require('total5');
 
 var path = '--bundles--';
 
 function buildplugin(name, callback) {
 	console.log('| |--', name + '.bundle');
-	BACKUP(path + '/' + name + '.bundle', PATH.root(), callback, function(path, isdir) {
+	Total.backup(path + '/' + name + '.bundle', PATH.root(), callback, function(path, isdir) {
 		return path === '/' || path === '/plugins/' || (path.indexOf('plugins/' + name) !== -1);
 	});
 }
@@ -15,7 +15,7 @@ console.log('|-- Total.js bundle compiler');
 console.time('|-- Compilation');
 
 console.log('| |--', 'app.bundle');
-BACKUP(path + '/app.bundle', PATH.root(), function() {
+Total.backup(path + '/app.bundle', PATH.root(), function() {
 	F.Fs.readdir(PATH.root('plugins'), function(err, response) {
 		response.wait(function(key, next) {
 
