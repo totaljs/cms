d6e378e776c049b2fb64a4b82a97a5e43ef5ec36
Peter Širka <petersirka@gmail.com>
2024-03-21 22:15:15 +0100

    Improved code.

--- controllers/default.js
+++ controllers/default.js
@@ -10,14 +10,13 @@ exports.install = function() {
 
 function admin($) {
 
-	var plugins = [];
-
 	if ($.user.openplatform && !$.user.iframe && $.query.openplatform) {
 		$.cookie(CONF.op_cookie, $.query.openplatform, NOW.add('12 hours'));
 		$.redirect($.url);
 		return;
 	}
 
+	var plugins = [];
 	var hostname = $.hostname();
 
 	if (CONF.url !== hostname)
