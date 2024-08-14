f8d5741d44752122c40a03ba0305697ae18fb4ed
petersirka <petersirka@gmail.com>
2024-08-14 21:07:11 +0200

    Updated UI components.

--- definitions/init.js
+++ definitions/init.js
@@ -22,7 +22,7 @@ ON('ready', function() {
 	setTimeout(FUNC.load, 500);
 
 	// UI components
-	COMPONENTATOR('ui', 'aselected,fileuploader,locale,exec,page,viewbox,navlayout,extend,crop,form,importer,input,box,validate,loading,selected,intranetcss,prompt,notify,message,errorhandler,empty,menu,autofill,enter,dropfiles,breadcrumb,virtualwire,noscrollbar,preview,miniform,datagrid,filebrowser,approve,shortcuts,searchdata,search,searchinput,display,selection,children,icons,directory,colorpicker,cloudeditor,tangular-filesize,textboxlist,datepicker,cloudeditorsimple,ready,listing,keyvalue,configuration,donutchart,statsbarsimple,stats24,barchart,clipboard,edit,choose,imageviewer,tangular-rgba,features,datasource,cl,autocomplete,filereader', true);
-	COMPONENTATOR('webui', 'exec,errorhandler,locale,uibuilder,uistudio,directory,message,approve,datepicker,menu,icons,input,imageviewer,mobilecarousel', true);
+	COMPONENTATOR('ui', 'aselected,fileuploader,locale,exec,page,viewbox,navlayout,extend,crop,form,importer,input,box,validate,loading,selected,intranetcss,prompt,notify,message,errorhandler,empty,menu,autofill,enter,dropfiles,breadcrumb,virtualwire,noscrollbar,preview,miniform,datagrid,filebrowser,approve,shortcuts,searchdata,search,searchinput,display,selection,children,icons,directory,colorpicker,cloudeditor,tangular-filesize,textboxlist,datepicker,cloudeditorsimple,ready,listing,keyvalue,configuration,donutchart,stats24,statsbarsimple,barchart,clipboard,edit,choose,imageviewer,tangular-rgba,features,datasource,cl,autocomplete,filereader', true);
+	COMPONENTATOR('webui', 'errorhandler,exec,locale,uibuilder,uistudio,directory,message,approve,datepicker,menu,icons,input,imageviewer,mobilecarousel', true);
 
 });
